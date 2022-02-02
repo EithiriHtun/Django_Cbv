5857846f65ac18796b5975622b6cfc09671b161f
eithiri <eithiritun1997@gmail.com>
2022-02-02 15:25:10 +0630

    feat: Section 26: Advanced Topics - Customizing the Django Admin

--- CbvApp/admin.py
+++ CbvApp/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from CbvApp.models import School,Student
 
+class StudentAdmin(admin.ModelAdmin):
+
+    fields = ['school','name','age']
+
+    search_fileds = ['name','school']
+
+    list_filter = ['name','age','school']
+
+    list_display = ['school','name','age']
+
+    list_editable = ['age']
+
 # Register your models here.
 admin.site.register(School)
-admin.site.register(Student)
+admin.site.register(Student,StudentAdmin)
